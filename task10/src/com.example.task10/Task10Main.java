c737390511f70002eefec168b01f008661b3e01f
Maksim Lyubimov <sackliem@mail.ru>
2024-09-12 22:25:24 +0500

    init

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -11,8 +11,15 @@ public class Task10Main {
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int min_index = 0;
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (min >= arr[i]){
+                min = arr[i];
+                min_index = i;
+            }
+        }
+        return min_index;
     }
 
 }
